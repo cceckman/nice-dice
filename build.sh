f3a8c2e62e0cbc00ba283f1f772f6a1dcaa98c94
Charles Eckman <charles@cceckman.com>
2025-07-07 12:23:31 -0400

    Include charts.css in the build step

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@
 set -eu
 wasm-pack build --target=web >&2
 
+cp -r pkg/ ~/r/github.com/cceckman/web/static/writing/dicer/
+cp app.js ~/r/github.com/cceckman/web/static/writing/dicer/
+cp demo.html ~/r/github.com/cceckman/web/static/writing/dicer/
+mkdir -p ~/r/github.com/cceckman/web/static/writing/dicer/charts.css-1.1.0/dist
+cp charts.css-1.1.0/dist/charts.min.css ~/r/github.com/cceckman/web/static/writing/dicer/charts.css-1.1.0/dist/charts.min.css
